682b65ddf3fb6361d1e7a42f926aacd1bc08ed28
dy6694074 <97041352+dy6694074@users.noreply.github.com>
2022-06-25 16:39:19 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1><HeaderIcon/>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
